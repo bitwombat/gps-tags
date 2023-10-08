5d3bd9fed3c94353ec0664637965e124d83d2826
Greg Bell <greg@bitwombat.com.au>
2023-10-08 15:07:56 +1100

    Working

--- app/main.go
+++ app/main.go
@@ -6,24 +6,111 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/bitwombat/tag/sub"
 )
 
+type TagData struct {
+	SerNo   int       `json:"SerNo"`
+	Imei    string    `json:"IMEI"`
+	Iccid   string    `json:"ICCID"`
+	ProdID  int       `json:"ProdId"`
+	Fw      string    `json:"FW"`
+	Records []Records `json:"Records"`
+}
+
+type AnalogueData struct {
+	Num1 int `json:"1"`
+	Num3 int `json:"3"`
+	Num4 int `json:"4"`
+	Num5 int `json:"5"`
+}
+
+type Fields struct {
+	GpsUTC       string       `json:"GpsUTC,omitempty"`
+	Lat          float64      `json:"Lat,omitempty"`
+	Long         float64      `json:"Long,omitempty"`
+	Alt          int          `json:"Alt,omitempty"`
+	Spd          int          `json:"Spd,omitempty"`
+	SpdAcc       int          `json:"SpdAcc,omitempty"`
+	Head         int          `json:"Head,omitempty"`
+	Pdop         int          `json:"PDOP,omitempty"`
+	PosAcc       int          `json:"PosAcc,omitempty"`
+	GpsStat      int          `json:"GpsStat,omitempty"`
+	FType        int          `json:"FType"`
+	DIn          int          `json:"DIn,omitempty"`
+	DOut         int          `json:"DOut,omitempty"`
+	DevStat      int          `json:"DevStat,omitempty"`
+	AnalogueData AnalogueData `json:"AnalogueData,omitempty"`
+}
+
+type Records struct {
+	SeqNo   int      `json:"SeqNo"`
+	Reason  int      `json:"Reason"`
+	DateUTC string   `json:"DateUTC"`
+	Fields  []Fields `json:"Fields"`
+}
+
+var lastWasHealthCheck bool
+
 func main() {
+
+	lastWasHealthCheck = false
+
 	httpMux := http.NewServeMux()
 	httpMux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "OK")
-		log.Println("Got a health check.")
+		if !lastWasHealthCheck {
+			log.Println("Got a health check [repeats will be hidden].")
+		}
+
+		lastWasHealthCheck = true
 	})
 
 	httpsMux := http.NewServeMux()
 	httpsMux.HandleFunc("/map", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Got a map page request.")
-		mapPage, err := sub.GetContents("public_html/index.html", map[string]string{
-			"tuckerPositions": `{lat:-31.457435,lng:152.641985}`,
-		})
+		lastWasHealthCheck = false
+
+		//------
+		body, err := ioutil.ReadFile("810095")
+		if err != nil {
+			log.Printf("Error reading 810095 (Rueger) file: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		subs := make(map[string]string)
+
+		fields := strings.Split(string(body), " ")
+		if len(fields) != 2 {
+			log.Printf("Error in format of 810095 file. Length of fields is %d, expected 2", len(fields))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		subs["ruegerPositions"] = fmt.Sprintf("{lat: %s, lng: %s}", fields[0], fields[1])
+
+		//------
+		body, err = ioutil.ReadFile("810243")
+		if err != nil {
+			log.Printf("Error reading 810243 (Tucker) file: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		fields = strings.Split(string(body), " ")
+		if len(fields) != 2 {
+			log.Printf("Error in format of 810243 file. Length of fields is %d, expected 2", len(fields))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		subs["tuckerPositions"] = fmt.Sprintf("{lat: %s, lng: %s}", fields[0], fields[1])
+
+		mapPage, err := sub.GetContents("public_html/index.html", subs)
 
 		if err != nil {
 			log.Printf("Error getting contents: %v\n", err)
@@ -64,6 +151,8 @@ func main() {
 		}
 
 		log.Println("Got a data post!")
+		lastWasHealthCheck = false
+
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Printf("Error reading body: %v", err)
@@ -73,19 +162,32 @@ func main() {
 
 		log.Println(string(body))
 
-		var result map[string]interface{}
+		var result TagData
 
-		err = json.Unmarshal(body, result)
+		err = json.Unmarshal(body, &result)
 		if err != nil {
 			log.Printf("Error unmarshalling JSON: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		if value, ok := result["SerNo"]; ok {
-			log.Printf("Value of 'SerNo': %v\n", value)
-		} else {
-			fmt.Println("Key 'SerNo' does not exist.")
+		log.Println(result.SerNo)
+		log.Printf("Got %d Records, keeping last one\n", len(result.Records))
+
+		for i, r := range result.Records {
+			log.Println(r.DateUTC)
+			gpsField := r.Fields[0]
+			log.Printf("   %0.7f, %0.7f\n", gpsField.Lat, gpsField.Long)
+
+			if i == len(result.Records)-1 {
+				err = ioutil.WriteFile(fmt.Sprintf("%d", result.SerNo), []byte(fmt.Sprintf("%0.7f %0.7f", gpsField.Lat, gpsField.Long)), 0644)
+			}
+
+			if err != nil {
+				log.Printf("Error writing to file: %v", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
 		}
 
 		w.WriteHeader(http.StatusOK)
