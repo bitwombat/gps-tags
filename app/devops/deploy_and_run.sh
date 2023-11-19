fdbd7789db1f80ee808ae3da3753b441fd722cd6
Greg Bell <greg@bitwombat.com.au>
2023-11-20 08:50:43 +1100

    Make order smarter.

--- app/devops/deploy_and_run.sh
+++ app/devops/deploy_and_run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 set -e
-echo "Stopping service"
-ssh -q proxy 'systemctl stop dog-tracking.service'
 
 echo "Building app"
 go build -ldflags="-s -w" main.go
+
+echo "Stopping service"
+ssh -q proxy 'systemctl stop dog-tracking.service'
 
 echo "Transferring service definition"
 scp -C devops/dog-tracking.service proxy:/etc/systemd/system/dog-tracking.service
 
 echo "Transferring web pages"
 scp -C ../public_html/* proxy:public_html
+
 echo "Transferring application binary"
 scp -C main proxy:.
 
