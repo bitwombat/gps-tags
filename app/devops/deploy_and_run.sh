82007b1e35b571e52380ef9bf270483d19247583
Greg Bell <greg@bitwombat.com.au>
2023-11-26 12:11:14 +1100

    Quiet ssh.

--- app/devops/deploy_and_run.sh
+++ app/devops/deploy_and_run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 set -e
+export RSYNC_RSH="ssh -q"
 
-RSYNC='rsync -r --compress --itemize-changes '
+export RSYNC_OPTS="-r --times --no-owner --delete --compress --itemize-changes "
 
 echo "Building app"
 go build -ldflags="-s -w" main.go
 
 echo "Transferring assets"
-$RSYNC ../{zones,public_html} proxy:
+rsync $RSYNC_OPTS ../{zones,public_html} proxy:
 
 echo "Transferring service definition"
-$RSYNC devops/dog-tracking.service proxy:/etc/systemd/system/dog-tracking.service
+rsync $RSYNC_OPTS devops/dog-tracking.service proxy:/etc/systemd/system/dog-tracking.service
 
 echo "Reloading systemctl daemon"
 ssh -q proxy systemctl daemon-reload
