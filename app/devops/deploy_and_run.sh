04a84afb48a4af1a3aebc967a6097cc63013b9d5
Greg Bell <greg@bitwombat.com.au>
2023-11-30 16:40:46 +1100

    feat: Soften deployment script.

--- app/devops/deploy_and_run.sh
+++ app/devops/deploy_and_run.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
 
-VPS_FQDN=proxy
-
 set -e
+
+# Vars
+SERVICE_FILE="dog-tracking.service"
+
+
+# Commands
 export RSYNC_RSH="ssh -q"
+export RSYNC_OPTS="-r --times --no-owner --exclude=public_html/log.html --delete --compress --itemize-changes "
 
-export RSYNC_OPTS="-r --times --no-owner --exclude="public_html/log.html" --delete --compress --itemize-changes "
 
+# Read and check .env file
+pushd devops &> /dev/null
+if [[ ! -e .env ]]; then
+    echo "ERROR: .env file not found. Please copy .env.example to .env and then edit it with your details."
+    exit 1
+fi
+
+. .env
+
+if [[ -z "$VPS_FQDN" ]]; then
+    echo "ERROR: VPS_FQDN not set in .env file."
+    exit 1
+fi
+popd &> /dev/null
+
+
+# Build app
 echo "Building app"
-pushd service
+pushd service &> /dev/null
 go build -ldflags="-s -w" .
-popd
+popd &> /dev/null
 
+
+# Copy everything over to VPS and restart service
 echo "Transferring assets"
 rsync $RSYNC_OPTS ./{named_zones,public_html} "$VPS_FQDN":
 
 echo "Transferring service definition"
-rsync $RSYNC_OPTS devops/dog-tracking.service "$VPS_FQDN":/etc/systemd/system/dog-tracking.service
+rsync $RSYNC_OPTS devops/"$SERVICE_FILE" "$VPS_FQDN":/etc/systemd/system/"$SERVICE_FILE"
 
 echo "Reloading systemctl daemon"
 ssh -q "$VPS_FQDN" systemctl daemon-reload
 
 echo "Stopping service"
-ssh -q "$VPS_FQDN" 'systemctl stop dog-tracking.service'
+ssh -q "$VPS_FQDN" "systemctl stop $SERVICE_FILE"
 
 echo "Transferring application binary"
 scp -C service/gps-tags "$VPS_FQDN":.
 
 echo "Starting service"
-ssh -q "$VPS_FQDN" 'systemctl start dog-tracking.service'
+ssh -q "$VPS_FQDN" "systemctl start $SERVICE_FILE"
