54ef6be8be428aced55426a0f4d786213d17a06a
Greg Bell <greg@bitwombat.com.au>
2025-09-07 22:18:57 +1000

    convert makefile to script

---
+++ app/service/cmd/migrate/make_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DBNAME="dogtags.db"
+JSON="dogs_pre_20250711.json"
+SQL=$(basename "$JSON" .json).sql
+
+if [ ! -x "$(which sqlite3 2>/dev/null)" ]; then
+    echo "Can't find sqlite3 executable"
+    exit 1
+fi
+
+[ -e "$DBNAME" ] && rm -i "$DBNAME"
+
+go run main.go "$JSON"
+
+echo "importing old data (slow)"
+cat "$SQL" | sqlite3 -bail "$DBNAME"
